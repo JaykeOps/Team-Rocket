c8c473ed0efcf3f1f56c2a3d0b74b721dbbce443
Kusken <jallowski@hotmail.se>
2016-10-29 00:03:09 +0200

    Finished initial work on the dateOfBirth value object. TODO: Need to write tests

---
+++ Domain/Value Objects/DateOfBirth.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace Domain.Value_Objects
+{
+    public class DateOfBirth
+    {
+        public DateOfBirth(string dateOfbirth)
+        {
+            if (this.IsValid(dateOfbirth))
+            {
+                this.Value = Convert.ToDateTime(dateOfbirth);
+            }
+        }
+
+        public DateTime Value { get; }
+
+        public bool IsValid(string value)
+        {
+            DateTime result;
+            if (DateTime.TryParseExact(value, "yyyy-MM-dd", CultureInfo.InvariantCulture,
+                DateTimeStyles.None, out result))
+            {
+                return !this.IsFuture(result) && !this.IsMoreThanAHundredYearsOld(result) ?
+                    true : false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool IsFuture(DateTime dateTime)
+        {
+            return dateTime > DateTime.Now ? true : false;
+        }
+
+        private bool IsMoreThanAHundredYearsOld(DateTime dateTime)
+        {
+            return dateTime.Year < DateTime.Now.Year - 100 ? true : false;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Value: yyyy-MM-dd}";
+        }
+    }
+}
