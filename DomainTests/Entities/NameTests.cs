03638e00c9252fcead2acc839ced9c0b47e0362d
Kusken <jallowski@hotmail.se>
2016-10-28 22:50:05 +0200

    Contains several tests of for the name value object

---
+++ DomainTests/Entities/NameTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using football_series_manager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace football_series_manager.Domain.Entities.Tests
+{
+    [TestClass()]
+    public class NameTests
+    {
+        [TestMethod]
+        public void NameIsEqualToEntry()
+        {
+            var name = new Name("Donald", "Trump");
+            Assert.IsTrue(name.FirstName == "Donald" && name.LastName == "Trump");
+        }
+
+        [TestMethod()]
+        public void NameIsNotNull()
+        {
+            var name = new Name("Hillary", "Clinton");
+            Assert.IsNotNull(name);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(FormatException))]
+        public void NameThatContainsNoneNumeralInvalidCharactersThrowsFormatException()
+        {
+            var name = new Name(".%&#/", ")(##¤%");
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(FormatException))]
+        public void NameThatContainsNumbersThrowsFormatException()
+        {
+            var name = new Name(".%&#/", ")(##¤%");
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(FormatException))]
+        public void NameThatContainsLessThanFourCharactersThrowsFormatException()
+        {
+            var name = new Name("h", "p");
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(FormatException))]
+        public void NameThatContainsMoreThanFourtyCharactersThrowsFormatException()
+        {
+            var name = new Name("Helloeveryoneletsseeifthisispassed", "Wonderifthistestwillbepassedornot");
+        }
+
+        [TestMethod()]
+        public void FirstNameIsValid()
+        {
+            Name.IsValid("Donald");
+        }
+
+        [TestMethod()]
+        public void LastNameIsValid()
+        {
+            Name.IsValid("Trump");
+        }
+
+    }
+}
