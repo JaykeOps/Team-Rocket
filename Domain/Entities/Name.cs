3e0ebaab64a907796f9a0d6ef5652035762f5328
Kusken <jallowski@hotmail.se>
2016-10-28 22:06:28 +0200

    First commit personAndPlayer branch - 1. Made a name value object that need testing 2. Made an interface for value objects forcing them to implement validation.  3. Made a half-finished skeleton of the abstract class Person.

---
+++ Domain/Entities/Name.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace football_series_manager.Domain.Entities
+{
+    public class Name
+    {
+        public Name(string firstName, string lastName)
+        {
+            if (IsValid(firstName) && IsValid(lastName))
+            {
+                this.FirstName = firstName;
+                this.LastName = lastName;
+            }
+            else
+            {
+                throw new FormatException("First/last name entry did not follow format restrictions!\n" +
+                    "A first/last name must consist of 2-20 letters.");
+            }
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool IsValid(string value)
+        {
+            return Regex.IsMatch(value, @"^[A-ZÅÄÖ][a-zåäö]{2,20}$");
+        }
+    }
+}
---
+++ Domain/Entities/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace football_series_manager.Domain.Entities
+{
+    public abstract class Person
+    {
+        public Person()
+        {
+
+        }
+
+        public Guid Id { get; set; }
+        public Name Name { get; set; }
+    }
+}
