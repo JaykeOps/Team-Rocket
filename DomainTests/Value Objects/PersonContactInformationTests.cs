2d14352f129926fd89e5b4d2dddecba9349c2951
Kusken <jallowski@hotmail.se>
2016-10-31 23:55:35 +0100

    Set all Player properties to read-write so that Players can be administrated and wrote related tests.

--- DomainTests/Value Objects/PersonContactInformationTests.cs
+++ DomainTests/Value Objects/PersonContactInformationTests.cs
@@ -5,15 +5,34 @@ namespace Domain.Value_Objects.Tests
     [TestClass()]
     public class PersonContactInformationTests
     {
-        [TestMethod()]
-        public void PersonContactInformationCanBeInitiatedWithValidArguments()
+        private ContactInformation contactInformation;
+
+        public PersonContactInformationTests()
         {
             var phone = new PhoneNumber("0734-668899");
             var email = new EmailAddress("johnDoe@outlook.com");
-            var contactInformation = new ContactInformation(phone, email);
+            this.contactInformation = new ContactInformation(phone, email);
+        }
 
+        [TestMethod()]
+        public void PersonContactInformationCanBeInitiatedWithValidArguments()
+        {
             Assert.IsTrue(contactInformation.Phone.Value == "0734-668899"
                 && contactInformation.Email.Value == "johnDoe@outlook.com");
+        }
+
+        [TestMethod()]
+        public void PersonContactInformationPhoneNumberCanChange()
+        {
+            this.contactInformation.Phone = new PhoneNumber("0739-884477");
+            Assert.IsTrue(this.contactInformation.Phone.Value == "0739-884477");
+        }
+
+        [TestMethod()]
+        public void PersonContactInformationEmailAddressCanChange()
+        {
+            this.contactInformation.Email = new EmailAddress("marco_polo@gm.com");
+            Assert.IsTrue(this.contactInformation.Email.Value == "marco_polo@gm.com");
         }
     }
 }
