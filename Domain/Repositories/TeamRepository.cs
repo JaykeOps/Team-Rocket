aa1fee4108cbd3d19371531f315f6fc5fdea8d22
ooleoole <olapopp85@gmail.com>
2016-11-01 09:57:35 +0100

    Worked on team repo and service

---
+++ Domain/Repositories/TeamRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Domain.Repositories
+{
+    public sealed class TeamRepository
+    {
+
+        public List<Team> Teams { get; }
+        public static TeamRepository Instance = new TeamRepository();
+
+        private TeamRepository()
+        {
+            this.Teams = new List<Team>();
+        }
+
+        public void Add(Team team)
+        {
+            this.Teams.Add(team);
+        }
+
+        private void Load()
+        {
+        }
+
+        internal void Save()
+        {
+        }
+    }
+}
---
+++ Domain/Services/TeamService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.Repositories;
+
+namespace Domain.Services
+{
+    public class TeamService
+    {
+        private readonly TeamRepository repository = TeamRepository.Instance;
+
+        public void AddTeam(Team team)
+        {
+           this.repository.Add(team);
+        }
+
+    }
+}
