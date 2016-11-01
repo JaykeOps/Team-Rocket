0f9069ae8158bb40e6a526c6b76343b0d75ad9e3
Jimmy Lundberg <jimmy-lundberg@hotmail.com>
2016-11-01 09:46:28 +0100

    GameClassesAdded

---
+++ Domain/Value Objects/MatchMinute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Domain.Value_Objects
+{
+    class MatchMinute
+    {
+        public int Value { get; }
+
+        public MatchMinute(int value)
+        {
+            if (IsMatchMinute(value))
+            {
+                this.Value = value;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        private bool IsMatchMinute(int value)
+        {
+            bool isMM = false;
+            // Validation...
+            return isMM;
+        }
+    }
+}
