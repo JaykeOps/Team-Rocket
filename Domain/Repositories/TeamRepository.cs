fe22e608d1fa5b5690e8ca38137d58400f5c40ff
Alem Putes <alem.iths@gmail.com>
2016-11-08 14:08:16 +0100

    Added TeamData in Repo.

--- Domain/Repositories/TeamRepository.cs
+++ Domain/Repositories/TeamRepository.cs
@@ -11,14 +11,8 @@ namespace Domain.Repositories
 
         private TeamRepository()
         {
-            this.teams = new List<Team>
-            {
-                new Team(new TeamName("Ifk Göteborg"),new ArenaName("Ullevi"),new EmailAddress("ifkgoteborg@gmail.com")),
-                new Team(new TeamName("Bk Häcken"),new ArenaName("BravidaArena"),new EmailAddress("hacken@gmail.com")),
-                new Team(new TeamName("Ifk Norrköping"),new ArenaName("Östgötaporten"),new EmailAddress("ifknorrkoping@gmail.com")),
-                new Team(new TeamName("Kalmar FF"),new ArenaName("GuldfågelnArena"),new EmailAddress("kff@gmail.com")),
-                new Team(new TeamName("Sundsvalls If"),new ArenaName("Norrporten"),new EmailAddress("sundsvall@gmail.com"))
-            };
+            this.teams = new List<Team>();
+            LoadData();
         }
 
         public void Add(Team team)
@@ -29,6 +23,25 @@ namespace Domain.Repositories
         public IEnumerable<Team> GetAll()
         {
             return this.teams;
+        }
+
+        public void LoadData()
+        {
+            var ManchesterUnited = new Team(new TeamName("Manchester United"), new ArenaName("Old Trafford"), new EmailAddress("ManchesterUnited@gmail.com"));
+            var RealMadrid = new Team(new TeamName("Real Madrid"), new ArenaName("Santiago Bernabeu"), new EmailAddress("RealMadrid@gmail.com"));
+            var FCBarcelona = new Team(new TeamName("FC Barcelona"), new ArenaName("Camp Noud"), new EmailAddress("FCBarcelona@gmail.com"));
+            var Arsenal = new Team(new TeamName("Arsenal"), new ArenaName("Emirates Stadium"), new EmailAddress("Arsenal@gmail.com"));
+            var Chelsea = new Team(new TeamName("Chelsea"), new ArenaName("Stamford Bridge"), new EmailAddress("Chelsea@gmail.com"));
+            var BayernMunchen = new Team(new TeamName("Bayern Munchen"), new ArenaName("Allianz Arena"), new EmailAddress("BayernMunchen@gmail.com"));
+            var ManchesterCity = new Team(new TeamName("Manchester City"), new ArenaName("Etihad Stadium"), new EmailAddress("ManchesterCity@gmail.com"));
+
+            teams.Add(ManchesterUnited);
+            teams.Add(RealMadrid);
+            teams.Add(FCBarcelona);
+            teams.Add(Arsenal);
+            teams.Add(Chelsea);
+            teams.Add(BayernMunchen);
+            teams.Add(ManchesterCity);
         }
     }
 }
