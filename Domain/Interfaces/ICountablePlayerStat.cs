42398f978fcf06c9cf24ac2ec01d84039fbde8d6
Kusken <jallowski@hotmail.se>
2016-11-08 22:08:57 +0100

    Wrote an interface for PlayerStats and added properties for red and yellow cards.

---
+++ Domain/Interfaces/ICountablePlayerStat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface ICountablePlayerStat
+    {
+        int GoalCount { get; }
+        int AssistCount { get; }
+        int YellowCardCount { get; }
+        int RedCardCount { get; }
+        int PenaltyCount { get; }
+    }
+}
--- Domain/Value Objects/PlayerStats.cs
+++ Domain/Value Objects/PlayerStats.cs
@@ -1,22 +1,30 @@
-﻿using Domain.Value_Objects;
+﻿using Domain.Interfaces;
+using Domain.Value_Objects;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DomainTests.Entities
 {
-    public class PlayerStats : ValueObject<PlayerStats>
+    public class PlayerStats : ValueObject<PlayerStats>, ICountablePlayerStat
     {
         private List<Goal> goalStats;
         private List<Assist> assistStats;
         private List<Card> cardStats;
         private List<Penalty> penaltyStats;
-
         public List<Goal> GoalStats { get { return goalStats; } }
         public List<Assist> AssistStats { get { return assistStats; } }
         public List<Card> CardStats { get { return cardStats; } }
         public List<Penalty> PenaltyStats { get { return penaltyStats; } }
         public int GoalCount { get { return goalStats.Count; } }
         public int AssistCount { get { return assistStats.Count; } }
-        public int CardCount { get { return cardStats.Count; } }
+        public int YellowCardCount
+        {
+            get { return cardStats.FindAll(x => x.CardType.Equals(CardType.Yellow)).Count; }
+        }
+        public int RedCardCount
+        {
+            get { return cardStats.FindAll(x => x.CardType.Equals(CardType.Red)).Count; }
+        }
         public int PenaltyCount { get { return penaltyStats.Count; } }
 
         public PlayerStats()
