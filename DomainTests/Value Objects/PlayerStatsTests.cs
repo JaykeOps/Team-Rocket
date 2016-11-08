292ea9f4a137e678f537b03c98c0cabc9994e9c2
Kusken <jallowski@hotmail.se>
2016-11-08 15:04:18 +0100

    test file

---
+++ DomainTests/Value Objects/PlayerStatsTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DomainTests.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Value_Objects;
+
+namespace DomainTests.Entities.Tests
+{
+    [TestClass]
+    public class PlayerStatsTests
+    {
+        PlayerStats playerStatsOne;
+        PlayerStats playerStatsTwo;
+        public PlayerStatsTests()
+        {
+            Player
+            playerStatsOne = new PlayerStats();
+            playerStatsTwo = new PlayerStats();
+        }
+        [TestMethod]
+        public void PlayerStatsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
