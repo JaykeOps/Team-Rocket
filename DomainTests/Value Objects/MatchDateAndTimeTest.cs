03f9e100a6be4aa7a2fa14893c3f5c5ed5db5fde
ooleoole <olapopp85@gmail.com>
2016-11-09 00:12:14 +0100

    AddedSomeMoreTest

--- DomainTests/Value Objects/MatchDateAndTimeTest.cs
+++ DomainTests/Value Objects/MatchDateAndTimeTest.cs
@@ -11,6 +11,10 @@ namespace DomainTests.Value_Objects
     [TestClass]
     public class MatchDateAndTimeTest
     {
+        private MatchDateAndTime dateOne = new MatchDateAndTime(new DateTime(2017, 01, 01, 19, 30, 00));
+        private MatchDateAndTime dateTwo = new MatchDateAndTime(new DateTime(2017, 01, 01, 19, 30, 00));
+        private MatchDateAndTime dateThree = new MatchDateAndTime(new DateTime(2017, 11, 01, 19, 30, 00));
+
         [TestMethod]
         public void MatchDurationIsEqualToEntry()
         {
@@ -29,12 +33,12 @@ namespace DomainTests.Value_Objects
 
         [TestMethod]
         [ExpectedException(typeof(FormatException))]
-        public void MatchDurationThrowsFormatExceptionOnTimeInPast()
+        public void MatchDateAndTimeThrowsFormatExceptionOnIsPast()
         {
             var matchDateAndTime = new MatchDateAndTime(DateTime.Now - TimeSpan.FromDays(1));
         }
         [TestMethod]
-        public void MatchDurationThrowsFormatExceptionOnTimeToYearsFuture()
+        public void MatchDateAndTimeThrowsFormatExceptionOnTimeToYearsFuture()
         {
             var matchDateAndTime = new MatchDateAndTime(DateTime.Now + TimeSpan.FromDays(366));
         }
@@ -59,12 +63,35 @@ namespace DomainTests.Value_Objects
             Assert.IsTrue($"{result.Value:yyyy-MM-dd HH:mm}" == "2016-12-02 19:30");
         }
         [TestMethod]
-        public void MatchDurationTryParseCanOutNull()
+        public void MatchDateAndTimeTryParseCanOutNull()
         {
             MatchDateAndTime result;
             MatchDateAndTime.TryParse("2014-12-02", out result);
             Assert.IsNull(result);
         }
+
+        [TestMethod]
+        public void MatchDateAndTimeIsComparableByValue()
+        {
+            Assert.AreEqual(dateOne, dateTwo);
+            Assert.AreNotEqual(dateOne, dateThree);
+        }
+
+        [TestMethod]
+        public void MatchDateAndTimeOperatorComparisonByValueTest()
+        {
+            Assert.IsTrue(dateOne == dateTwo);
+            Assert.IsTrue(dateOne != dateThree);
+        }
+
+        [TestMethod]
+        public void MatchDateAndTimeWorksWithHashSet()
+        {
+            var matchHashSet = new HashSet<MatchDateAndTime> { dateOne, dateTwo };
+            Assert.IsTrue(matchHashSet.Count==1);
+        }
+
+
 
 
     }
