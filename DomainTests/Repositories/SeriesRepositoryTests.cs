65edacfb05ceb322ff409f894236b97b1d489bbd
Sebastian <Sebastian>
2016-11-08 15:46:53 +0100

    Added SeriesRepository and SeriesService and some tests.

---
+++ DomainTests/Repositories/SeriesRepositoryTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain.Services;
+using Domain.Entities;
+using Domain.Value_Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainTests.Repositories
+{
+    [TestClass()]
+    public class SeriesRepositoryTests
+    {
+        [TestMethod()]
+        public void AddSeries()
+        {
+            //Don't know what to test here.
+            //TODO: Add tests(?).
+        }
+    }
+}
