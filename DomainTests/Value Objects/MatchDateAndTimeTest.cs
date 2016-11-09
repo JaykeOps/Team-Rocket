427cbaabdf646c6eb1d229342bd650232d959acc
ooleoole <olapopp85@gmail.com>
2016-11-09 09:17:00 +0100

    minor refactoring

--- DomainTests/Value Objects/MatchDateAndTimeTest.cs
+++ DomainTests/Value Objects/MatchDateAndTimeTest.cs
@@ -37,11 +37,13 @@ namespace DomainTests.Value_Objects
         {
             var matchDateAndTime = new MatchDateAndTime(DateTime.Now - TimeSpan.FromDays(1));
         }
+
         [TestMethod]
         public void MatchDateAndTimeThrowsFormatExceptionOnTimeToYearsFuture()
         {
             var matchDateAndTime = new MatchDateAndTime(DateTime.Now + TimeSpan.FromDays(366));
         }
+
         [TestMethod]
         public void MatchDateAndTimeTryParseCanReturnTrue()
         {
@@ -55,6 +57,7 @@ namespace DomainTests.Value_Objects
             MatchDateAndTime result;
             Assert.IsFalse(MatchDateAndTime.TryParse("2020-12-02 19:30", out result));
         }
+
         [TestMethod]
         public void MatchDateAndTimeCanOutValidResult()
         {
@@ -62,6 +65,7 @@ namespace DomainTests.Value_Objects
             MatchDateAndTime.TryParse("2016-12-02 19:30", out result);
             Assert.IsTrue($"{result.Value:yyyy-MM-dd HH:mm}" == "2016-12-02 19:30");
         }
+
         [TestMethod]
         public void MatchDateAndTimeTryParseCanOutNull()
         {
