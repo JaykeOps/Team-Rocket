93b80542f22106adecad88d5ff67405aec89d0eb
Kusken <jallowski@hotmail.se>
2016-11-10 10:39:07 +0100

    Passing in this.TeamIds instead of teamIds

--- Domain/Entities/Game.cs
+++ Domain/Entities/Game.cs
@@ -8,13 +8,9 @@ namespace Domain.Entities
     {
         public Guid Id { get; }
         public MatchDuration MatchDuration { get; }
-        public Guid HomeTeamId { get; }     // Need to keep track of which team is hometeam and which is awayteam as it matters for
-        public Guid AwayTeamId { get; }     // the order in which GameProtocol entries are written - hometeam entries to the left.
+        public Guid HomeTeamId { get; }
+        public Guid AwayTeamId { get; }
         public GameProtocol Protocol { get; }
-        // public HashSet<Guid> TeamIds { get; } // Contains the ids for the two teams playing the game. Why Ids?
-
-        //public Dictionary<Team, HashSet<Guid>> TeamSet { get; } // Contains Team as key and the collection of that team's player ids as value.
-        //                                                        // Two entries, one for each team in the game.
 
         public Game(MatchDuration matchDuration, Guid homeTeamId, Guid awayTeamId)
         {
@@ -22,9 +18,7 @@ namespace Domain.Entities
             this.MatchDuration = matchDuration;
             this.HomeTeamId = homeTeamId;
             this.AwayTeamId = awayTeamId;
-            // this.TeamIds.Add(homeTeam.Id);
-            // this.TeamIds.Add(awayTeam.Id);
-            this.Protocol = new GameProtocol(homeTeamId, awayTeamId); // More arguments may be needed...
+            this.Protocol = new GameProtocol(this.HomeTeamId, this.AwayTeamId);
         }
     }
 }
