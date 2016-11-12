2cd031afe371819d5ee278815560ec2634357427
Kusken <jallowski@hotmail.se>
2016-11-12 12:26:25 +0100

    Wrote FindById methods for all services except match services, needs testing...

--- Domain/Services/DomainService.cs
+++ Domain/Services/DomainService.cs
@@ -1,13 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Entities;
+using System;
 
 namespace Domain.Services
 {
-    internal static class DomainService
+    public static class DomainService
     {
+        public static Player FindPlayerById(Guid id)
+        {
+            var playerService = new PlayerService();
+            return playerService.FindById(id);
+        }
 
+        public static Team FindTeamById(Guid id)
+        {
+            var teamService = new TeamService();
+            return teamService.FindById(id);
+        }
+
+        public static Game FindGameById(Guid id)
+        {
+            var gameService = new GameService();
+            return gameService.FindById(id);
+        }
+
+        //internal static Match FindMatchById(Guid id)
+        //{
+        //    var matchService = new MatchService();
+
+        //}
+
+        public static Series FindSeriesById(Guid id)
+        {
+            var seriesService = new SeriesService();
+            return seriesService.FindById(id);
+        }
     }
 }
---
+++ DomainTests/Services/DomainServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services.Tests
+{
+    [TestClass]
+    public class DomainServiceTests
+    {
+        [TestMethod]
+        public void FindPlayerByIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void FindTeamByIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void FindGameByIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void FindSeriesByIdTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
