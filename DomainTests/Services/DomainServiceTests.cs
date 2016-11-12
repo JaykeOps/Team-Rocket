5646aab22aa5f6ae5656a5c24cba2953931379b5
Kusken <jallowski@hotmail.se>
2016-11-12 15:56:32 +0100

    Wrote tests for all FindById methods. All tests pass except for the FindMatchById since its repo doesnt contain any data!

--- DomainTests/Services/DomainServiceTests.cs
+++ DomainTests/Services/DomainServiceTests.cs
@@ -1,38 +1,75 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Domain.Services;
+﻿using Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.Services.Tests
 {
     [TestClass]
     public class DomainServiceTests
     {
-        [TestMethod]
-        public void FindPlayerByIdTest()
+        private PlayerService playerService;
+        private SeriesService seriesService;
+        private TeamService teamService;
+        private MatchService matchService;
+        private GameService gameService;
+        private Player playerOne;
+        private Team teamOne;
+        private Game gameOne;
+        private Match matchOne;
+        private Series seriesOne;
+        private Guid playerId;
+        private Guid teamId;
+        private Guid gameId;
+        private Guid matchId;
+        private Guid seriesId;
+
+        [TestInitialize]
+        public void Init()
         {
-            Assert.Fail();
+            this.playerService = new PlayerService();
+            this.seriesService = new SeriesService();
+            this.teamService = new TeamService();
+            this.gameService = new GameService();
+            this.matchService = new MatchService();
+            this.playerOne = this.playerService.GetAll().ToList().First();
+            this.teamOne = this.teamService.GetAll().ToList().First();
+            this.gameOne = this.gameService.GetAll().ToList().First();
+            //this.matchOne = this.matchService.GetAll().ToList().First();
+            this.seriesOne = this.seriesService.GetAll().ToList().First();
+            this.playerId = this.playerOne.Id;
+            this.teamId = this.teamOne.Id;
+            this.gameId = this.gameOne.Id;
+            //this.matchId = this.matchOne.Id;
+            this.seriesId = this.seriesOne.Id;
         }
 
         [TestMethod]
-        public void FindTeamByIdTest()
+        public void FindPlayerByIdCanReturnPlayer()
         {
-            Assert.Fail();
+            var playerId = this.playerService.FindById(this.playerId).Id;
+            Assert.AreEqual(playerId, this.playerOne.Id);
+        }
+
+        [TestMethod]
+        public void FindTeamByIdCanReturnTeam()
+        {
+            var teamId = this.teamService.FindById(this.teamId).Id;
+            Assert.AreEqual(teamId, this.teamOne.Id);
         }
 
         [TestMethod]
         public void FindGameByIdTest()
         {
-            Assert.Fail();
+            var gameId = this.gameService.FindById(this.gameId).Id;
+            Assert.AreEqual(gameId, this.gameOne.Id);
         }
 
         [TestMethod]
         public void FindSeriesByIdTest()
         {
-            Assert.Fail();
+            var seriesId = this.seriesService.FindById(this.seriesId).Id;
+            Assert.AreEqual(seriesId, this.seriesOne.Id);
         }
     }
 }
