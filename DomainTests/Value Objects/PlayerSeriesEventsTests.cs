ef1c9c4bffb57a0fec7af79c616c28a8b2d51f01
Kusken <jallowski@hotmail.se>
2016-11-20 10:27:34 +0100

    Setup dummy!

--- DomainTests/Value Objects/PlayerSeriesEventsTests.cs
+++ DomainTests/Value Objects/PlayerSeriesEventsTests.cs
@@ -6,51 +6,33 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Linq;
 using Domain.Value_Objects;
+using DomainTests.Test_Dummies;
 
 namespace DomainTests.Value_Objects
 {
     [TestClass]
     public class PlayerSeriesEventsTests
     {
-        private Player playerOne;
+        private DummySeries dummySeries;
         private Team teamOne;
-        private Series seriesOne;
-        private IEnumerable<Match> teamOneSeriesOneMatchSchedule;
-        private GameService gameService;
-        private Game gameOne;
-        private Game gameTwo;
-        private Game gameThree;
-        private Game gameFour;
+        private Player playerOne;
 
         [TestInitialize]
         public void Init()
         {
-            this.seriesOne = DomainService.GetAllSeries().First();
-            var schedule = new Schedule();
-            schedule.GenerateSchedule(this.seriesOne);
-            this.teamOne = DomainService.FindTeamById(this.seriesOne.TeamIds.First());
-            this.teamOneSeriesOneMatchSchedule = this.teamOne.MatchSchedules[this.seriesOne.Id];
-            this.playerOne = this.teamOne.Players.First();
-            this.gameService = new GameService();
-            this.gameOne = new Game(this.teamOneSeriesOneMatchSchedule.First());
-            this.gameTwo = new Game(this.teamOneSeriesOneMatchSchedule.ElementAt(1));
-            this.gameThree = new Game(this.teamOneSeriesOneMatchSchedule.ElementAt(2));
-            this.gameFour = new Game(this.teamOneSeriesOneMatchSchedule.ElementAt(3));
-            this.gameService.Add(this.gameOne);
-            this.gameService.Add(this.gameTwo);
-            this.gameService.Add(this.gameThree);
-            this.gameService.Add(this.gameFour);
-            DomainService.AddSeriesToPlayers(this.seriesOne, this.teamOne.Players);
+            this.dummySeries = new DummySeries();
+            this.teamOne = this.dummySeries.DummyTeams.DummyTeamOne;
+            this.playerOne = this.dummySeries.DummyTeams.DummyTeamOne.Players.ElementAt(0);
         }
 
         [TestMethod]
         public void PlayerSeriesEventsIsUpdateWhenNewGoalIsAdded()
         {
-            this.teamOneSeriesOneMatchSchedule.First();
-            Assert.IsTrue(this.playerOne.SeriesEvents[this.seriesOne.Id] != null);
-            var playerOneGoalsPreAdd = this.playerOne.PresentableSeriesEvents[this.seriesOne.Id].Goals.Count();
-            this.gameOne.Protocol.Goals.Add(new Goal(new MatchMinute(14), this.playerOne.TeamId, playerOne.Id));
-            var playerOneGoalsPostAdd = this.playerOne.SeriesEvents[this.seriesOne.Id].Goals.Count();
+            
+            Assert.IsNotNull(this.playerOne.SeriesEvents[this.dummySeries.SeriesDummy.Id]);
+            var playerOneGoalsPreAdd = this.playerOne.PresentableSeriesEvents[this.dummySeries.SeriesDummy.Id].Goals.Count();
+            this.dummySeries.DummyGames.GameTwo.Protocol.Goals.Add(new Goal(new MatchMinute(14), this.playerOne.TeamId, this.playerOne.Id));
+            var playerOneGoalsPostAdd = this.playerOne.SeriesEvents[this.dummySeries.SeriesDummy.Id].Goals.Count();
             Assert.IsTrue(playerOneGoalsPostAdd - playerOneGoalsPreAdd == 1);
         }
     }
