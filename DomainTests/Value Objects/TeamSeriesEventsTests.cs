60abb23738dd9b0b242104c477617631f96a9125
Kusken <jallowski@hotmail.se>
2016-11-20 17:19:10 +0100

    Added tests that checks that team score makes correct calculations based on team events --> team stats wins, draws, losses!

--- DomainTests/Value Objects/TeamSeriesEventsTests.cs
+++ DomainTests/Value Objects/TeamSeriesEventsTests.cs
@@ -1,7 +1,6 @@
 ﻿using Domain.Entities;
 using DomainTests.Test_Dummies;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Domain.Value_Objects.Tests
@@ -10,44 +9,37 @@ namespace Domain.Value_Objects.Tests
     public class TeamSeriesEventsTests
     {
         private DummySeries dummySeries;
-        private Team teamFour;
+        private Team teamTwo;
 
         [TestInitialize]
         public void Init()
         {
             this.dummySeries = new DummySeries();
-            this.teamFour = this.dummySeries.DummyTeams.DummyTeamFour;
+            this.teamTwo = this.dummySeries.DummyTeams.DummyTeamTwo;
         }
 
         [TestMethod]
         public void TeamSeriesEventGamesIsNotNull()
         {
-            Assert.IsNotNull(this.teamFour.SeriesEvents[this.dummySeries.SeriesDummy.Id].Games.Count());
+            Assert.IsNotNull(this.teamTwo.SeriesEvents[this.dummySeries.SeriesDummy.Id].Games);
         }
 
         [TestMethod]
-        public IEnumerable<Game> TeamSeriesEventGamesCanBeAccessed()
-        {
-            var games = this.teamFour.SeriesEvents[this.dummySeries.SeriesDummy.Id].Games;
-            Assert.IsNotNull(games);
-            return games;
-        }
-
-        [TestMethod()]
         public void TeamSeriesStatsCanShowCorrectTeamWinCountBasedOnEvents()
         {
+            var games = this.teamTwo.SeriesEvents[this.dummySeries.SeriesDummy.Id].Games;
+            Assert.IsNotNull(games);
             var gamesWon = 0;
-            var games = this.TeamSeriesEventGamesCanBeAccessed();
             foreach (var game in games)
             {
-                if (game.Protocol.HomeTeamId == this.teamFour.Id)
+                if (game.Protocol.HomeTeamId == this.teamTwo.Id)
                 {
                     if (game.Protocol.GameResult.HomeTeam_Score > game.Protocol.GameResult.AwayTeam_Score)
                     {
                         gamesWon++;
                     }
                 }
-                else if (game.Protocol.AwayTeamId == this.teamFour.Id)
+                else if (game.Protocol.AwayTeamId == this.teamTwo.Id)
                 {
                     if (game.Protocol.GameResult.HomeTeam_Score < game.Protocol.GameResult.AwayTeam_Score)
                     {
@@ -55,9 +47,75 @@ namespace Domain.Value_Objects.Tests
                     }
                 }
             }
-            var teamWins = this.teamFour.PresentableSeriesStats[this.dummySeries.SeriesDummy.Id].Wins;
+            var teamWins = this.teamTwo.PresentableSeriesStats[this.dummySeries.SeriesDummy.Id].Wins;
             Assert.IsTrue(teamWins != 0);
             Assert.IsTrue(gamesWon == teamWins);
+        }
+
+        [TestMethod]
+        public void TeamSeriesStatsCanShowCorrectTeamDrawCountBasedOnEvents()
+        {
+            var games = this.teamTwo.SeriesEvents[this.dummySeries.SeriesDummy.Id].Games;
+            Assert.IsNotNull(games);
+            var gamesDraw = 0;
+            foreach (var game in games)
+            {
+                if (game.Protocol.HomeTeamId == this.teamTwo.Id)
+                {
+                    if (game.Protocol.GameResult.HomeTeam_Score == game.Protocol.GameResult.AwayTeam_Score)
+                    {
+                        gamesDraw++;
+                    }
+                }
+                else if (game.Protocol.AwayTeamId == this.teamTwo.Id)
+                {
+                    if (game.Protocol.GameResult.HomeTeam_Score == game.Protocol.GameResult.AwayTeam_Score)
+                    {
+                        gamesDraw++;
+                    }
+                }
+            }
+            var teamDraws = this.teamTwo.PresentableSeriesStats[this.dummySeries.SeriesDummy.Id].Draws;
+            Assert.IsTrue(teamDraws != 0);
+            Assert.IsTrue(gamesDraw == teamDraws);
+        }
+
+        [TestMethod]
+        public void TeamSeriesStatsCanShowCorrectTeamLossCountBasedOnEvents()
+        {
+            var games = this.teamTwo.SeriesEvents[this.dummySeries.SeriesDummy.Id].Games;
+            Assert.IsNotNull(games);
+            var gamesLost = 0;
+            foreach (var game in games)
+            {
+                if (game.Protocol.HomeTeamId == this.teamTwo.Id)
+                {
+                    if (game.Protocol.GameResult.HomeTeam_Score < game.Protocol.GameResult.AwayTeam_Score)
+                    {
+                        gamesLost++;
+                    }
+                }
+                else if (game.Protocol.AwayTeamId == this.teamTwo.Id)
+                {
+                    if (game.Protocol.GameResult.HomeTeam_Score > game.Protocol.GameResult.AwayTeam_Score)
+                    {
+                        gamesLost++;
+                    }
+                }
+            }
+            var teamLosses = this.teamTwo.PresentableSeriesStats[this.dummySeries.SeriesDummy.Id].Losses;
+            Assert.IsTrue(teamLosses != 0);
+            Assert.IsTrue(gamesLost == teamLosses);
+        }
+
+        [TestMethod]
+        public void TeamStatsCanShowCorrectTeamScoreBasedOnEvents()
+        {
+            var teamWins = this.teamTwo.PresentableSeriesStats[this.dummySeries.SeriesDummy.Id].Wins;
+            var teamDraws = this.teamTwo.PresentableSeriesStats[this.dummySeries.SeriesDummy.Id].Draws;
+            var teamLosses = this.teamTwo.PresentableSeriesStats[this.dummySeries.SeriesDummy.Id].Losses;
+            var teamScore = this.teamTwo.PresentableSeriesStats[this.dummySeries.SeriesDummy.Id].Points;
+            Assert.IsTrue(teamScore == (3 * teamWins) + (1 * teamDraws) + (0 * teamLosses));
         }
     }
 }
