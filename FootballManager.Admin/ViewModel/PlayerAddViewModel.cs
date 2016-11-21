0e1400b2b844bd2de207ea9e4cebb46c4b5c7024
Alem Putes <alem.iths@gmail.com>
2016-11-21 20:55:56 +0100

    Added Admin Gui to GUI-New branch

---
+++ FootballManager.Admin/ViewModel/PlayerAddViewModel.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Domain.Entities;
+using Domain.Services;
+using Domain.Value_Objects;
+using FootballManager.Admin.Extensions;
+using FootballManager.Admin.Utility;
+using FootballManager.Admin.View;
+
+
+namespace FootballManager.Admin.ViewModel
+{
+    public class PlayerAddViewModel : ViewModelBase
+    {
+        private TeamService teamService;
+        private Player player;
+
+        private int shirtNumber;
+        private string firstName;
+        private string lastName;
+        private string dateOfBirth;
+        private PlayerPosition selectedPlayerPosition;
+        private PlayerStatus selectedPlayerStatus;
+        private Team selectedTeam;
+
+        public PlayerAddViewModel()
+        {           
+            this.teamService = new TeamService();
+
+            this.PlayerAddCommand = new RelayCommand(PlayerAdd);
+        }
+
+
+        #region Properties
+        public ICommand PlayerAddCommand { get; }
+
+        public int ShirtNumber
+        {
+            get { return shirtNumber; }
+            set
+            {
+                if (shirtNumber != value)
+                {
+                    shirtNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (firstName != value)
+                {
+                    firstName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (lastName != value)
+                {
+                    lastName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string DateOfBirth
+        {
+            get { return dateOfBirth; }
+            set
+            {
+                if (dateOfBirth != value)
+                {
+                    dateOfBirth = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        #endregion
+
+        #region ComboBox properties
+
+        public PlayerPosition SelectedPlayerPosition
+        {
+            get { return selectedPlayerPosition; }
+            set
+            {
+                if (selectedPlayerPosition != value)
+                {
+                    selectedPlayerPosition = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public PlayerStatus SelectedPlayerStatus
+        {
+            get { return selectedPlayerStatus; }
+            set
+            {
+                if (selectedPlayerStatus != value)
+                {
+                    selectedPlayerStatus = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public Team SelectedTeam
+        {
+            get { return selectedTeam; }
+            set
+            {
+                if (selectedTeam != value)
+                {
+                    selectedTeam = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        #endregion
+
+        #region ComboBox population
+        public IEnumerable<PlayerPosition> PlayerPosition
+        {
+            get { return Enum.GetValues(typeof(PlayerPosition)).Cast<PlayerPosition>(); }
+        }
+
+        public IEnumerable<PlayerStatus> PlayerStatus
+        {
+            get { return Enum.GetValues(typeof(PlayerStatus)).Cast<PlayerStatus>(); }
+        }
+
+        public IEnumerable<Team> PlayerTeams
+        {
+            get { return teamService.GetAll(); }
+        }
+        #endregion
+
+        #region Methods
+        private void PlayerAdd(object obj)
+        {
+            this.player = new Player(new Name(firstName, lastName), new DateOfBirth(dateOfBirth), selectedPlayerPosition, selectedPlayerStatus);
+            player.TeamId = selectedTeam.Id;
+            player.ShirtNumber = new ShirtNumber(player.TeamId, shirtNumber);
+
+            Messenger.Default.Send<Player>(player);
+        }
+        #endregion
+    }
+}
---
+++ FootballManager.Admin/ViewModel/TeamAddViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Domain.Entities;
+using Domain.Services;
+using Domain.Value_Objects;
+using FootballManager.Admin.Utility;
+
+namespace FootballManager.Admin.ViewModel
+{
+    public class TeamAddViewModel : ViewModelBase
+    {        
+        private Team team;
+
+        private string teamName;
+        private string arenaName;
+        private string email;
+
+        public ICommand AddCommand { get; }
+
+        public TeamAddViewModel()
+        {
+            this.AddCommand = new RelayCommand(Add);
+        }
+
+        private void Add(object obj)
+        {
+            team.Name = new TeamName(teamName);
+            team.ArenaName = new ArenaName(arenaName);
+            team.Email = new EmailAddress(email);
+
+            Messenger.Default.Send<Team>(team);
+        }
+
+
+        #region Team Properties       
+        public string TeamName
+        {
+            get { return teamName; }
+            set
+            {
+                if (teamName != value)
+                {
+                    teamName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string ArenaName
+        {
+            get { return arenaName; }
+            set
+            {
+                if (arenaName != value)
+                {
+                    arenaName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                if (email != value)
+                {
+                    email = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+    }
+}
