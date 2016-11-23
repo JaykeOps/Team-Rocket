9411da2c01ff083afe7bff4cf6def1ec408abf05
ooleoole <olapopp85@gmail.com>
2016-11-23 21:14:42 +0100

    1

--- DomainTests/Services/TeamServiceTests.cs
+++ DomainTests/Services/TeamServiceTests.cs
@@ -62,7 +62,6 @@ namespace DomainTests.Services
             var teamStats = service.GetTeamStatsInSeries(series.SeriesDummy.Id, team.Id);
             var games = DomainService.GetAllGames();
             var teamGoals = games.Where(game => game.SeriesId == series.SeriesDummy.Id).SelectMany(game => game.Protocol.Goals).Count(goal => goal.TeamId == team.Id);
-
             Assert.IsTrue(teamGoals == teamStats.GoalsFor);
         }
     }
