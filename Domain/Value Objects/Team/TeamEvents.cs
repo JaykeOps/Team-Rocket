c1f293bb68ed32c220822bcad1dfa7ff1adca1df
ooleoole <olapopp85@gmail.com>
2016-11-28 10:29:04 +0100

    1

--- Domain/Value Objects/Team/TeamEvents.cs
+++ Domain/Value Objects/Team/TeamEvents.cs
@@ -14,6 +14,7 @@ namespace Domain.Value_Objects
         private IEnumerable<Game> games;
         private IEnumerable<Goal> goals;
 
+
         public string TeamName => this.teamName;
         public IEnumerable<Game> Games => this.games;
         public IEnumerable<Goal> Goals => this.goals;
