04f0b14097022e48658e2abb62cf3732de34b242
Jimmy Lundberg <jimmy-lundberg@hotmail.com>
2016-11-29 11:56:20 +0100

    Commit to backup solution before temporary implementation changes for troubleshooting purposes.

--- FootballManager.App/ViewModel/SeriesViewModel.cs
+++ FootballManager.App/ViewModel/SeriesViewModel.cs
@@ -9,11 +9,37 @@ using Domain.Services;
 using FootballManager.App.Extensions;
 using FootballManager.App.Utility;
 using FootballManager.App.View;
+using Dragablz;
 
 namespace FootballManager.App.ViewModel
 {
     public class SeriesViewModel
     {
+        private SeriesService seriesService;
 
+        public SeriesViewModel()
+        {
+            seriesService = new SeriesService();
+        }
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region Combobox population
+
+        public IEnumerable<Series> AllSeries
+        {
+            get
+            {
+                return seriesService.GetAll(); // This will later be called as a WCF service.
+            }
+        }
+
+        #endregion
     }
 }
