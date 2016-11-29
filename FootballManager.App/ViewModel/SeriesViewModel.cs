3af6399884a88ededce992c417ed0aa06a8c8238
Jimmy Lundberg <jimmy-lundberg@hotmail.com>
2016-11-29 17:43:29 +0100

    Added combobox binding for choose series.

--- FootballManager.App/ViewModel/SeriesViewModel.cs
+++ FootballManager.App/ViewModel/SeriesViewModel.cs
@@ -3,17 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows;
 using System.Windows.Input;
 using Domain.Entities;
 using Domain.Services;
+using Domain.Value_Objects;
 using FootballManager.App.Extensions;
 using FootballManager.App.Utility;
 using FootballManager.App.View;
+using Dragablz;
 
 namespace FootballManager.App.ViewModel
 {
     public class SeriesViewModel
     {
+        private SeriesService seriesService;
 
+        public SeriesViewModel()
+        {
+            seriesService = new SeriesService();
+        }
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region Combobox population
+
+
+        public IEnumerable<SeriesName> AllSeries
+        {
+            get { return seriesService.GetAll().Select(s => s.SeriesName); }
+        }
+
+        #endregion
     }
 }
