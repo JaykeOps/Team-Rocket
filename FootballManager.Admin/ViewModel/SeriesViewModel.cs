2751176006d97b0e011adac8f9a3a5ab29f90256
Jimmy Lundberg <jimmy-lundberg@hotmail.com>
2016-12-01 17:31:15 +0100

    Frontend-validation works on SeriesName!

--- FootballManager.Admin/ViewModel/SeriesViewModel.cs
+++ FootballManager.Admin/ViewModel/SeriesViewModel.cs
@@ -7,11 +7,61 @@ using System.Windows.Input;
 using FootballManager.Admin.Utility;
 using FootballManager.Admin.View;
 using MaterialDesignThemes.Wpf;
+using System.ComponentModel;
+using Domain.Helper_Classes;
 
 namespace FootballManager.Admin.ViewModel
 {
-    public class SeriesViewModel : ViewModelBase
+    public class SeriesViewModel : ViewModelBase, IDataErrorInfo
     {
+        private string seriesName;
 
+        public SeriesViewModel()
+        {
+        }
+
+        #region IDataErrorInfo implementation
+        public string Error
+        {
+            get { return null; }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                switch (columnName)
+                {
+                    case "SeriesName":
+                        if (this.SeriesName == null || this.SeriesName == "")
+                        {
+                            return string.Empty;
+                        }
+                        if (!this.SeriesName.IsValidSeriesName(false)) // Parameter is 'bool ignoreCase'.
+                        {
+                            return "Must be 2-30 characters long!";
+                        }
+                        break;
+                }
+                return string.Empty;
+            }
+        }
+        #endregion
+
+        #region Properties
+        public string SeriesName
+        {
+            get { return seriesName; }
+            set
+            {
+                seriesName = value;
+                base.OnPropertyChanged("SeriesName");
+            }
+        }
+        #endregion
+
+        #region Methods
+
+        #endregion
     }
 }
