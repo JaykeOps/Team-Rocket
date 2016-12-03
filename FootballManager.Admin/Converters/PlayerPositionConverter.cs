adc37445daeaa344c0383f44f73fccf7a85406ed
Kusken <jallowski@hotmail.se>
2016-12-03 14:27:11 +0100

    EditPlayer in team info player position now functions as wanted!

--- FootballManager.Admin/Converters/PlayerPositionConverter.cs
+++ FootballManager.Admin/Converters/PlayerPositionConverter.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
 
 namespace FootballManager.Admin.Converters
@@ -12,21 +8,13 @@ namespace FootballManager.Admin.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var position = (PlayerPosition) value;
+            var position = (PlayerPosition)value;
             return position == PlayerPosition.NonAssigned ? "n/a" : position.ToString();
-
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value == null)
-            {
-                return PlayerPosition.NonAssigned;
-            }
-            else
-            {
-                return (PlayerPosition) value;
-            }
+            return (string)value == "n/a" ? "NonAssigned" : value;
         }
     }
 }
