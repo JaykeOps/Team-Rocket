65b1c8c7347dd4027d594bb57642fce7eb41d197
ooleoole <olapopp85@gmail.com>
2016-12-05 20:41:08 +0100

    small fixexs

--- FootballManager.App/ViewModel/PlayerViewModel.cs
+++ FootballManager.App/ViewModel/PlayerViewModel.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Windows;
 using System.Windows.Input;
 using Domain.Entities;
 using Domain.Interfaces;
 using Domain.Services;
 using FootballManager.App.Extensions;
-using FootballManager.App.Utility;
-using FootballManager.App.View;
-using Dragablz;
 
 namespace FootballManager.App.ViewModel
 {
