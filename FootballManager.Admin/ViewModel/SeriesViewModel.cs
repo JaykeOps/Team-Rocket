f0380c11158b4dc8e34b8603a03803b71c2acbb8
Jimmy Lundberg <jimmy-lundberg@hotmail.com>
2016-12-07 20:04:15 +0100

    Minor changes: Removed one commented line of code and one unnecessary error message in SeriesViewModel.

--- FootballManager.Admin/ViewModel/SeriesViewModel.cs
+++ FootballManager.Admin/ViewModel/SeriesViewModel.cs
@@ -302,7 +302,7 @@ namespace FootballManager.Admin.ViewModel
                         {
                             validProperties[columnName] = false;
                             ValidateProperties();
-                            return "You have added too few teams to the list!";
+                            return string.Empty;
                         }
                         if ((this.TeamsToAddToSeries.Count() < this.SelectedNumberOfTeams))
                         {
@@ -321,7 +321,6 @@ namespace FootballManager.Admin.ViewModel
                 validProperties[columnName] = true;
                 ValidateProperties();
                 return string.Empty;
-                // return "Fuck this shit!";
             }
         }
 
